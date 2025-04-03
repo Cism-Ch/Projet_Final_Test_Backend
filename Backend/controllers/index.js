23d7b4a5e30107ae9b4818dbf684017bda4789a9
Cism-Ch <coulibaly.ismael.ch.0@gmail.com>
2025-04-03 12:47:56 +0000

    update 06

--- Backend/controllers/index.js
+++ Backend/controllers/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // User: require('./user.ctrl'),
+    User: require('./user.ctrl'),
     Task: require('./task.ctrl'),
 }
--- Backend/routes/task.route.js
+++ Backend/routes/task.route.js
@@ -6,4 +6,5 @@ taskRouter.put('/:id', taskCtrl.updateTask); // Update a task by ID
 taskRouter.get('/owner/:id', taskCtrl.getOwnerTasks); // Get tasks by owner ID
 taskRouter.delete('/:id', taskCtrl.deleteTask); // Delete a task by ID
 taskRouter.get('/findone/:id', taskCtrl.findOneTask); // Get a task by ID 'findOneTask'
+
 module.exports = taskRouter;
